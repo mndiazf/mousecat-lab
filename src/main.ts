e130d32aaf7e7e3f55f84654ff72422693dd74db
mndiazf <mn.diaz.f@gmail.com>
2025-05-21 21:14:03 -0400

    Revert "feat(header): implementar submenú móvil de servicios con animaciones"
    This reverts commit 1cff4bf6ae7e5cd2d18e7f5d165d571c80e0115d.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [...appConfig.providers, provideAnimations()],
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
