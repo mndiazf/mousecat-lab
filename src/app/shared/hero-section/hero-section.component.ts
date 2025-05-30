fe43c131d257c87d1faafcac18dc2d5c34926e24
mndiazf <mn.diaz.f@gmail.com>
2025-05-29 20:45:37 -0400

    header/hero-update
    whatsapp
    correo
    responsive hero
    optimizacion de codigo

--- src/app/shared/hero-section/hero-section.component.ts
+++ src/app/shared/hero-section/hero-section.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
 export class HeroSectionComponent {
   @Input() title: string = '';
+  @Input() showQuoteButton: boolean = true;
 }
